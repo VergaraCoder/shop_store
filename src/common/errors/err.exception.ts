af2a0c6c449fd6a58bcb70c84bb4a766b2f3646b
vergaraCoder <jhonatanvergara072@gmail.com>
2025-05-23 09:25:12 -0500

    feat: creation of the error exception

---
+++ src/common/errors/err.exception.ts
@@ -0,0 +1,37 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class ErrorFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const request = ctx.getRequest<Request>();
+    const response = ctx.getResponse<Response>();
+
+    console.log(exception);
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const message =
+      exception instanceof HttpException
+        ? exception.getResponse()
+        : 'INTERNAL SERVER ERROR';
+
+    response.status(status).json({
+      status,
+      timestamp: new Date().toISOString(),
+      method: request.method,
+      url: request.url,
+      message,
+    });
+  }
+}
